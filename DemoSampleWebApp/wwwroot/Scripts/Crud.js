ad044ceb99e9b140c77f56a955ea79a097f675fa
Pavan Kumar Pannala <pavankumar.pannala@outlook.com>
2019-03-28 09:58:04 +0530

    Added some code

---
+++ DemoSampleWebApp/wwwroot/Scripts/Crud.js
@@ -0,0 +1,53 @@
+﻿var newapp = angular.module('newApp', ['ui.grid']);
+
+newapp.controller('newController', function ($scope, $http) {
+    $scope.arrEmps = new Array;
+
+    getData();
+
+    function getData()
+    {
+        var request = {
+            method: 'GET',
+            url: 'https://demoapi-2-dev-as.azurewebsites.net/API/Persons',
+            dataType: 'json',
+            headers:
+            {
+                'Content-Type': 'application/json',
+                'api-version': '1.0'
+            }
+        };
+
+        $http(request).then(function (response) {
+            console.log('Get Response', response);
+            console.log('Get Response Data', response.data);
+            var i = 0;
+            angular.forEach(response.data,
+                function () {
+                    var emp =
+                    {
+                        Id: response[i].data.id,
+                        Name: response[i].data.name,
+                        Email: response[i].data.email
+                    };
+                    console.log('emp', emp);
+                    $scope.arrEmps.push(emp);
+                    i += 1;
+                },
+                function (error) {
+                    alert('Error');
+                });
+
+            alert($scope.arrEmps);
+            $scope.gridData = { data: 'arrEmps' };
+
+        });
+
+    }
+});
+
+
+        
+
+
+    
